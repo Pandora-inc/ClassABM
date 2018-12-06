48c47ce087c32f8378fb44d6c0a1bc4af17c7a6e
iberlot <ivanberlot@gmail.com>
2018-12-06 15:03:48 -0300

    F_C-001
    Incorporación de la lógica de campos a la generación de la búsqueda y del ordenamiento de la generación del listado.
    Se incorpora la función campoFormBuscar() en las clases donde corresponde.

--- funciones.php
+++ funciones.php
@@ -47,4 +47,39 @@ abstract class Funciones
 
 		return key ($array);
 	}
+
+	/**
+	 * Convierte de un array todas las entidades HTML para que sea seguro mostrar en pantalla strings ingresados por los usuarios
+	 *
+	 * @example $_REQUEST = limpiarEntidadesHTML($_REQUEST);
+	 *
+	 * @param String[] $param
+	 * @return String[] - Depende del parametro recibido, un array con los datos remplazados o un String
+	 */
+	public function limpiarEntidadesHTML($param, $sitio = "")
+	{
+		if (!isset ($sitio) and empty ($sitio))
+		{
+			global $sitio;
+		}
+
+		if (is_array ($param))
+		{
+			// Hay veces que devuelve error aca =(
+			return array_map ("Funciones::limpiarEntidadesHTML", $param);
+		}
+		else
+		{
+			if (isset ($sitio->charset))
+			{
+				return htmlentities ($param, ENT_QUOTES, $sitio->charset);
+			}
+			else
+			{
+				$param = htmlentities ($param);
+
+				return $param;
+			}
+		}
+	}
 }
