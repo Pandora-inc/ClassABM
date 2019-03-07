52dfc73bee6770f4e380265daa385d356fca5121
iberlot <ivanberlot@gmail.com>
2019-03-07 09:03:27 -0300

    MC-ABM-008-003

--- class_abm2.php
+++ class_abm2.php
@@ -3812,13 +3812,26 @@ class class_abm
 					}
 					else
 					{
+						print_r ("entro");
 						if ($campo->existeDato ("joinTable") and $campo->isOmitirJoin () == false)
 						{
+							print_r ("hasta adentro");
+
 							$tablaJoin = $campo->getJoinTable ();
 							$tablaJoin = explode (".", $tablaJoin);
 							$tablaJoin = $tablaJoin[count ($tablaJoin) - 1];
 
-							$campo->setCampo ($tablaJoin . "_" . $campo->getCampoTexto ());
+							if ($campo->existeDato ("campoTexto"))
+							{
+
+								print_r ("HAY");
+								$campo->setCampo ($tablaJoin . "_" . $campo->getCampoTexto ());
+							}
+							else
+							{
+								print_r ("HUY");
+								$campo->setCampo ($tablaJoin . "_" . $campo->getCampo ());
+							}
 						}
 					}
 
