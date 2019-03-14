d1bc940acdf998e5fd26feb23927e45fe316640a
iberlot <ivanberlot@gmail.com>
2019-03-14 15:35:07 -0300

    MC-ABM-009

--- class_abm.php
+++ class_abm.php
@@ -2362,7 +2362,7 @@ class class_abm
 				$imprForm = "";
 			}
 
-			$imprForm .= "<div class='content'>";
+			// $imprForm .= "<div class='content'>";
 			$imprForm .= "<section>\n";
 			$imprForm .= "<div id='form'>\n";
 
